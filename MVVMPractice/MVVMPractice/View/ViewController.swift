6af26e57c4fb0574876412ac4a581c5047045047
Kim DongHwan <kylo2001@naver.com>
2019-11-07 14:53:54 +0900

    프로젝트 설정

---
+++ MVVMPractice/MVVMPractice/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMPractice
+//
+//  Created by DongWhan on 2019/11/07.
+//  Copyright © 2019 Kim DongHwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
